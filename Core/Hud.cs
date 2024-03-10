9534cb7678f9fb7acd56952d560225743fdbc76a
faehromon <161216896+faehromon@users.noreply.github.com>
2024-03-10 09:10:31 +0100

    prebuild instruction texts

--- Core/Hud.cs
+++ Core/Hud.cs
@@ -9,6 +9,10 @@ namespace AlternativeCameraMod;
 
 internal class Hud
 {
+   const string InstructionPrefix = " ► ";
+   const string InstructionSeparator = " ├  ";
+   const string InstructionSeparatorLast = " └  ";
+   
    private readonly State _state;
    private readonly CameraControl _camera;
    private readonly InputHandler _input;
@@ -20,7 +24,9 @@ internal class Hud
    private int _fpsDisplayThreshold;
    private int _displayedFps;
    private bool _intialized;
-
+   private PlayModeHudInstructions _playModeInstr;
+   private PhotoModeHudInstructions _photoModeInstr;
+   
 
    public Hud(State state, CameraControl camera, InputHandler input, Configuration cfg,
                      LanguageConfig lang, Logger logger)
@@ -37,9 +43,11 @@ internal class Hud
    public void InitializeOnce()
    {
       if (_intialized) return;
-     
-      ParseHudInfoDisplayFlags(_cfg.PlayMode.ModHudInfoDisplay.Value);
 
+      PreBuildPlayModeInstructions();
+      PreBuildPhotoModeInstructions();
+
+      ParseHudInfoDisplayFlags(_cfg.PlayMode.ModHudInfoDisplay.Value);
       MelonEvents.OnGUI.Subscribe(DrawInfoOnHud, 100);
       _intialized = true;
    }
@@ -178,8 +186,28 @@ internal class Hud
       }
    }
 
+   
+   internal static string FormatLabel(string text, int size, string color, bool bold = false, bool italic = false)
+   {
+      string preFmt = "";
+      string postFmt = "";
+      if (bold)
+      {
+         preFmt += "<b>";
+         postFmt += "</b>";
+      }
+      if (italic)
+      {
+         preFmt += "<i>";
+         postFmt += "</i>";
+      }
+      
+      var str = String.Format("{3}<color={2}><size={1}>{0}</size></color>{4}", text, size, color, preFmt, postFmt);
+      return str;
+   }
 
-   private void DrawPlayModeInstructionsOnHud()
+
+   private void PreBuildPlayModeInstructions()
    {
       var actionListInOrderToShow = new List<PlayModeAction>
       {
@@ -203,22 +231,20 @@ internal class Hud
       };
 
       var actions = new StringBuilder();
-      string pre = " ► ";
+      
       actions.AppendLine(_lang.GetText("Input", "ActionHeader", "ACTION"));
       for (var index = 0; index < actionListInOrderToShow.Count; index++)
       {
          var action = actionListInOrderToShow[index];
-         actions.AppendLine(pre + _input.PlayMode.GetActionText(action));
+         actions.AppendLine(InstructionPrefix + _input.PlayMode.GetActionText(action));
       }
-
-      const string sep = " ├  ";
-      const string lastSep = " └  ";
+      
       var keys = new StringBuilder();
       keys.AppendLine(_lang.GetText("Input", "KeyMouseHeader", "KEYBOARD/MOUSE"));
       for (var index = 0; index < actionListInOrderToShow.Count; index++)
       {
          var action = actionListInOrderToShow[index];
-         keys.AppendLine((index < actionListInOrderToShow.Count - 1 ? sep : lastSep) + _input.PlayMode.GetKeyText(action));
+         keys.AppendLine((index < actionListInOrderToShow.Count - 1 ? InstructionSeparator : InstructionSeparatorLast) + _input.PlayMode.GetKeyText(action));
       }
       
       var btns = new StringBuilder();
@@ -226,60 +252,29 @@ internal class Hud
       for (var index = 0; index < actionListInOrderToShow.Count; index++)
       {
          var action = actionListInOrderToShow[index];
-         btns.AppendLine((index < actionListInOrderToShow.Count - 1 ? sep : lastSep) + _input.PlayMode.GetButtonText(action));
+         btns.AppendLine((index < actionListInOrderToShow.Count - 1 ? InstructionSeparator : InstructionSeparatorLast) + _input.PlayMode.GetButtonText(action));
       }
       
-      var titleForeColor = Configuration.GameColor2;
-      var foreColor = Color.white;
-      var shadowColor = "black";
-      var shadowOff = 2;
-
-      var textSize = 20;
-      var fmtBindDescr = FormatLabel(actions.ToString(), textSize, foreColor);
-      var fmtKeyDescr = FormatLabel(keys.ToString(), textSize, foreColor);
-      var fmtBtnDescr = FormatLabel(btns.ToString(), textSize, foreColor);
-
-      var fmtBindDescrShadow = FormatLabel(actions.ToString(), textSize, shadowColor);
-      var fmtKeyDescrShadow = FormatLabel(keys.ToString(), textSize, shadowColor);
-      var fmtBtnDescrShadow = FormatLabel(btns.ToString(), textSize, shadowColor);
-
       var pmLabel = _lang.GetText("PlayMode", "Title", "CONTROLS");
-      
-      var w1 = _lang.GetIntNum("PlayMode", "ColWidthAction", 450);
-      var w2 = _lang.GetIntNum("PlayMode", "ColWidthController", 200);
-      var w3 = _lang.GetIntNum("PlayMode", "ColWidthKeyMouse", 200);
 
-      var boxWidth = w1 + w2 + w3 + 40;
-      var boxHeight = 480;
-      var boxPadding = 20;
-
-      float xOffset = 850;
-      float xOffset2 = xOffset + w1;
-      float xOffset3 = xOffset2 + w2;
-
-      float yPosOffset = 280;
-      float yPosTitle = yPosOffset;
-      float yPosInstr = yPosOffset + 50;
-      
-      GUI.Box(new Rect(xOffset - boxPadding, yPosOffset - boxPadding, boxWidth + boxPadding*2, boxHeight + boxPadding*2), "");
-
-      GUI.Label(new Rect(xOffset + shadowOff, yPosTitle + shadowOff, 1000, 200), FormatLabel(pmLabel, 30, nameof(Color.black), bold: true));
-      GUI.Label(new Rect(xOffset, yPosTitle, 1000, 200), FormatLabel(pmLabel, 30, titleForeColor, bold: true));
-
-      GUI.Label(new Rect(xOffset + shadowOff, yPosInstr + shadowOff, 2000, 2000), fmtBindDescrShadow);
-      GUI.Label(new Rect(xOffset, yPosInstr, 2000, 2000), fmtBindDescr);
-
-      GUI.Label(new Rect(xOffset2 + shadowOff, yPosInstr + shadowOff, 2000, 2000), fmtKeyDescrShadow);
-      GUI.Label(new Rect(xOffset2, yPosInstr, 2000, 2000), fmtKeyDescr);
-
-      GUI.Label(new Rect(xOffset3 + shadowOff, yPosInstr + shadowOff, 2000, 2000), fmtBtnDescrShadow);
-      GUI.Label(new Rect(xOffset3, yPosInstr, 2000, 2000), fmtBtnDescr);
+      _playModeInstr = new PlayModeHudInstructions();
+      _playModeInstr.Build(
+         pmLabel,
+         actions.ToString(),
+         keys.ToString(),
+         btns.ToString(),
+         titleSize: 30, 
+         titleColor: Configuration.GameColor2, 
+         textSize: 20, 
+         textColor: nameof(Color.white), 
+         shadowColor: nameof(Color.black), 
+         shadowOffset: 2);
    }
 
-   
-   private void DrawPhotoModeInstructionsOnHud()
+
+   private void PreBuildPhotoModeInstructions()
    {
-       var actionListInOrderToShow = new List<PhotoModeAction>
+      var actionListInOrderToShow = new List<PhotoModeAction>
       {
          PhotoModeAction.Exit,
          PhotoModeAction.ShootPhoto,
@@ -295,22 +290,19 @@ internal class Hud
       };
 
       var actions = new StringBuilder();
-      string pre = " ► ";
       actions.AppendLine(_lang.GetText("Input", "ActionHeader", "ACTION"));
       for (var index = 0; index < actionListInOrderToShow.Count; index++)
       {
          var action = actionListInOrderToShow[index];
-         actions.AppendLine(pre + _input.PhotoMode.GetActionText(action));
+         actions.AppendLine(InstructionPrefix + _input.PhotoMode.GetActionText(action));
       }
-
-      const string sep = " ├  ";
-      const string lastSep = " └  ";
+      
       var keys = new StringBuilder();
       keys.AppendLine(_lang.GetText("Input", "KeyMouseHeader", "KEYBOARD/MOUSE"));
       for (var index = 0; index < actionListInOrderToShow.Count; index++)
       {
          var action = actionListInOrderToShow[index];
-         keys.AppendLine((index < actionListInOrderToShow.Count - 1 ? sep : lastSep) + _input.PhotoMode.GetKeyText(action));
+         keys.AppendLine((index < actionListInOrderToShow.Count - 1 ? InstructionSeparator : InstructionSeparatorLast) + _input.PhotoMode.GetKeyText(action));
       }
       
       var btns = new StringBuilder();
@@ -318,27 +310,69 @@ internal class Hud
       for (var index = 0; index < actionListInOrderToShow.Count; index++)
       {
          var action = actionListInOrderToShow[index];
-         btns.AppendLine((index < actionListInOrderToShow.Count - 1 ? sep : lastSep) + _input.PhotoMode.GetButtonText(action));
+         btns.AppendLine((index < actionListInOrderToShow.Count - 1 ? InstructionSeparator : InstructionSeparatorLast) + _input.PhotoMode.GetButtonText(action));
       }
       
-      var titleForeColor = Configuration.GameColor2;
-      var foreColor = Color.white;
-      var shadowColor = "black";
-      var shadowOff = 2;
-
-      var textSize = 20;
-      var fmtBindDescr = FormatLabel(actions.ToString(), textSize, foreColor);
-      var fmtKeyDescr = FormatLabel(keys.ToString(), textSize, foreColor);
-      var fmtBtnDescr = FormatLabel(btns.ToString(), textSize, foreColor);
-
-      var fmtBindDescrShadow = FormatLabel(actions.ToString(), textSize, shadowColor);
-      var fmtKeyDescrShadow = FormatLabel(keys.ToString(), textSize, shadowColor);
-      var fmtBtnDescrShadow = FormatLabel(btns.ToString(), textSize, shadowColor);
-
       var pmLabel = _lang.GetText("PhotoMode", "Title", "PHOTO MODE");
-      var focusModeLabel = _lang.GetText("PhotoMode", "FocusAdjustModeLabel_{state}", "Focus mode: {0}", GetFocusModeText());
-      var note = _lang.GetText("PhotoMode", "Note", "(This instructions box is not part of the photo)");
 
+      _photoModeInstr = new PhotoModeHudInstructions();
+      _photoModeInstr.Build(
+         pmLabel,
+         actions.ToString(),
+         keys.ToString(),
+         btns.ToString(),
+         titleSize: 30, 
+         titleColor: Configuration.GameColor2, 
+         textSize: 20, 
+         textColor: nameof(Color.white),
+      shadowColor: nameof(Color.black),
+      shadowOffset: 2);
+
+      _photoModeInstr.BuildNoteLabel(
+         _lang.GetText("PhotoMode", "Note", "(This instructions box is not part of the photo)"),
+         15,
+         "#CCCCCC",
+         nameof(Color.black));
+   }
+
+
+   private void WriteLabel(string text, string shadowText, float x, float y, float w,  float h, float shadowOffset = 2)
+   {
+      GUI.Label(new Rect(x + shadowOffset, y + shadowOffset, w, h), shadowText);
+      GUI.Label(new Rect(x, y, w, h), text);
+   }
+
+
+   private void DrawPlayModeInstructionsOnHud()
+   {
+      var w1 = _lang.GetIntNum("PlayMode", "ColWidthAction", 450);
+      var w2 = _lang.GetIntNum("PlayMode", "ColWidthController", 200);
+      var w3 = _lang.GetIntNum("PlayMode", "ColWidthKeyMouse", 200);
+
+      var boxWidth = w1 + w2 + w3 + 40;
+      var boxHeight = 480;
+      var boxPadding = 20;
+
+      float xPos = 850;
+      float xPos2 = xPos + w1;
+      float xPos3 = xPos2 + w2;
+
+      float yPosOffset = 280;
+      float yPosTitle = yPosOffset;
+      float yPosInstr = yPosOffset + 50;
+      
+      GUI.Box(new Rect(xPos - boxPadding, yPosOffset - boxPadding, boxWidth + boxPadding*2, boxHeight + boxPadding*2), "");
+
+      WriteLabel(_playModeInstr.Title, _playModeInstr.TitleShadow, xPos, yPosTitle, 1000, 200);
+      WriteLabel(_playModeInstr.Actions, _playModeInstr.ActionsShadow, xPos, yPosInstr, 2000, 2000);
+      WriteLabel(_playModeInstr.Keys, _playModeInstr.KeysShadow, xPos2, yPosInstr, 2000, 2000);
+      WriteLabel(_playModeInstr.Buttons, _playModeInstr.ButtonsShadow, xPos3, yPosInstr, 2000, 2000);
+   }
+
+   
+   private void DrawPhotoModeInstructionsOnHud()
+   {
+      var focusModeLabel = _lang.GetText("PhotoMode", "FocusAdjustModeLabel_{state}", "Focus mode: {0}", GetFocusModeText());
       var w1 = _lang.GetIntNum("PhotoMode", "ColWidthAction", 350);
       var w2 = _lang.GetIntNum("PhotoMode", "ColWidthController", 200);
       var w3 = _lang.GetIntNum("PhotoMode", "ColWidthKeyMouse", 200);
@@ -347,9 +381,9 @@ internal class Hud
       var boxHeight = 450;
       var boxPadding = 20;
 
-      float xOffset = 50;
-      float xOffset2 = xOffset + w1;
-      float xOffset3 = xOffset2 + w2;
+      float xPos = 50;
+      float xPos2 = xPos + w1;
+      float xPos3 = xPos2 + w2;
 
       float yPosTitle = 200;
       float yPosInstr = 250;
@@ -357,33 +391,24 @@ internal class Hud
       float yPosState = 600;
       float yPosSaveInfo = 680 + boxPadding;
 
-      GUI.Box(new Rect(xOffset - boxPadding, yPosTitle - boxPadding, boxWidth + boxPadding*2, boxHeight + boxPadding*2), "");
-
-      GUI.Label(new Rect(xOffset + shadowOff, yPosTitle + shadowOff, 1000, 200), FormatLabel(pmLabel, 30, nameof(Color.black), bold:true));
-      GUI.Label(new Rect(xOffset, yPosTitle, 1000, 200), FormatLabel(pmLabel, 30, titleForeColor, bold:true));
-
-      GUI.Label(new Rect(xOffset + shadowOff, yPosInstr + shadowOff, 2000, 2000), fmtBindDescrShadow);
-      GUI.Label(new Rect(xOffset, yPosInstr, 2000, 2000), fmtBindDescr);
-
-      GUI.Label(new Rect(xOffset2 + shadowOff, yPosInstr + shadowOff, 2000, 2000), fmtKeyDescrShadow);
-      GUI.Label(new Rect(xOffset2, yPosInstr, 2000, 2000), fmtKeyDescr);
-
-      GUI.Label(new Rect(xOffset3 + shadowOff, yPosInstr + shadowOff, 2000, 2000), fmtBtnDescrShadow);
-      GUI.Label(new Rect(xOffset3, yPosInstr, 2000, 2000), fmtBtnDescr);
-
-      GUI.Label(new Rect(xOffset + shadowOff, yPosState + shadowOff, 1000, 200), 
-         FormatLabel(focusModeLabel, 20, shadowColor));
-      GUI.Label(new Rect(xOffset, yPosState, 1000, 200), FormatLabel(focusModeLabel, 20, "lightblue"));
-
-      GUI.Label(new Rect(xOffset + 1, yPosNote + 1, 1000, 200), FormatLabel(note, 15, nameof(Color.black)));
-      GUI.Label(new Rect(xOffset, yPosNote, 1000, 200), FormatLabel(note, 15, "#CCCCCC"));
-
+      GUI.Box(new Rect(xPos - boxPadding, yPosTitle - boxPadding, boxWidth + boxPadding*2, boxHeight + boxPadding*2), "");
+      
+      WriteLabel(_photoModeInstr.Title, _photoModeInstr.TitleShadow, xPos, yPosTitle, 1000, 200);
+      WriteLabel(_photoModeInstr.Actions, _photoModeInstr.ActionsShadow, xPos, yPosInstr, 2000, 2000);
+      WriteLabel(_photoModeInstr.Keys, _photoModeInstr.KeysShadow, xPos2, yPosInstr, 2000, 2000);
+      WriteLabel(_photoModeInstr.Buttons, _photoModeInstr.ButtonsShadow, xPos3, yPosInstr, 2000, 2000);
+      
+      WriteLabel(_photoModeInstr.Note, _photoModeInstr.NoteShadow, xPos, yPosNote, 2000, 2000, 1);
+      WriteLabel(FormatLabel(focusModeLabel, 20, "lightblue"), FormatLabel(focusModeLabel, 20, "black"), xPos, yPosState, 2000, 2000, 2);
+      
       if (!string.IsNullOrEmpty(_state.LastScreenshotInfo))
       {
-         GUI.Box(new Rect(xOffset - boxPadding, yPosSaveInfo - boxPadding, boxWidth + boxPadding*2, 90), "");
+         GUI.Box(new Rect(xPos - boxPadding, yPosSaveInfo - boxPadding, boxWidth + boxPadding*2, 90), "");
 
-         GUI.Label(new Rect(xOffset + shadowOff, yPosSaveInfo + shadowOff, boxWidth, 200), FormatLabel(_state.LastScreenshotInfo, 20, shadowColor));
-         GUI.Label(new Rect(xOffset, yPosSaveInfo, boxWidth, 200), FormatLabel(_state.LastScreenshotInfo, 20, Configuration.GameColor2));
+         WriteLabel(
+            FormatLabel(_state.LastScreenshotInfo, 20, Configuration.GameColor2), 
+            FormatLabel(_state.LastScreenshotInfo, 20, "black"),
+            xPos, yPosSaveInfo, boxWidth, 200);
       }
    }
 
@@ -403,33 +428,7 @@ internal class Hud
       }
    }
 
-
-   private string FormatLabel(string text, int size, Color color, bool bold = false, bool italic = false)
-   {
-      return FormatLabel(text, size, color.ToString(), bold, italic);
-   }
-
-
-   private string FormatLabel(string text, int size, string color, bool bold = false, bool italic = false)
-   {
-      string preFmt = "";
-      string postFmt = "";
-      if (bold)
-      {
-         preFmt += "<b>";
-         postFmt += "</b>";
-      }
-      if (italic)
-      {
-         preFmt += "<i>";
-         postFmt += "</i>";
-      }
-      
-      var str = String.Format("{3}<color={2}><size={1}>{0}</size></color>{4}", text, size, color, preFmt, postFmt);
-      return str;
-   }
-
-
+   
    public static Camera? GetHudCam()
    {
       var hudCamObj = GameObject.Find("UICam");
---
+++ Core/HudInstructions.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+
+
+namespace AlternativeCameraMod;
+
+internal abstract class HudInstructions
+{
+   private Dictionary<string, string> _formats = new Dictionary<string, string>();
+
+   private string _title;
+   private string _titleShadow;
+   private string _actions;
+   private string _keys;
+   private string _buttons;
+   private string _actionsShadow;
+   private string _keysShadow;
+   private string _buttonsShadow;
+
+
+   public void Build(string title, string actions, string keys, string buttons, int titleSize, string titleColor, int textSize, string textColor, string shadowColor, int shadowOffset)
+   {
+      _title = Hud.FormatLabel(title, titleSize, titleColor, true);
+      _titleShadow = Hud.FormatLabel(title, titleSize, shadowColor, true);
+
+      _actions = Hud.FormatLabel(actions, textSize, textColor);
+      _keys = Hud.FormatLabel(keys, textSize, textColor);
+      _buttons = Hud.FormatLabel(buttons, textSize, textColor);
+
+      _actionsShadow = Hud.FormatLabel(actions, textSize, shadowColor);
+      _keysShadow = Hud.FormatLabel(keys, textSize, shadowColor);
+      _buttonsShadow = Hud.FormatLabel(buttons, textSize, shadowColor);
+   }
+
+
+   public string Title
+   {
+      get { return _title; }
+   }
+
+
+   public string TitleShadow
+   {
+      get { return _titleShadow; }
+   }
+
+
+   public string Actions
+   {
+      get { return _actions; }
+   }
+
+
+   public string Keys
+   {
+      get { return _keys; }
+   }
+
+
+   public string Buttons
+   {
+      get { return _buttons; }
+   }
+
+
+   public string ActionsShadow
+   {
+      get { return _actionsShadow; }
+   }
+
+
+   public string KeysShadow
+   {
+      get { return _keysShadow; }
+   }
+
+
+   public string ButtonsShadow
+   {
+      get { return _buttonsShadow; }
+   }
+}
+
+
+
+internal class PlayModeHudInstructions : HudInstructions
+{
+}
+
+
+
+internal class PhotoModeHudInstructions : HudInstructions
+{
+   private string _note;
+   private string _noteShadow;
+
+
+   public void BuildNoteLabel(string text, int size, string textColor, string shadowColor)
+   {
+      _note = Hud.FormatLabel(text, size, textColor);
+      _noteShadow = Hud.FormatLabel(text, size, shadowColor);
+   }
+
+
+   public string Note
+   {
+      get { return _note; }
+   }
+
+
+   public string NoteShadow
+   {
+      get { return _noteShadow; }
+   }
+}
